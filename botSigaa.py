2d8df5639490a776f248a45187439cba1984f033
Rayanne <rayanne_lima2010@hotmail.com>
2020-10-25 10:16:34 -0300

    Bot para o SIGAA - funcionalidades pendentes

--- botSigaa.py
+++ botSigaa.py
@@ -21,28 +21,48 @@ class sigaaBot:
         password_element.clear()
         password_element.send_keys(self.password)
         password_element.send_keys(Keys.RETURN)
-        time.sleep(5)
-        self.entrarMateria()
 
-    def entrarMateria(self):
+        time.sleep(5)
+
+        qntDisciplinas = self.quantidadeDisciplinas()
+        self.entrarPrimeiraDisciplina()
+        self.baixarMateriais()
+        self.trocarDisciplina(1)
+        for i in range(2, qntDisciplinas):
+            self.baixarMateriais()
+            self.trocarDisciplina(i)
+            print(i)
+
+    def quantidadeDisciplinas(self):
+        driver = self.driver
+        materias = driver.find_elements_by_xpath("//a[contains(@id,'form_acessarTurmaVirtual')]")
+        print("Quantidade de disciplinas: " + str(len(materias)))
+        return str(len(materias))
+
+    def entrarPrimeiraDisciplina(self):
         driver = self.driver
         driver.find_element_by_xpath("//a[@id='form_acessarTurmaVirtual:turmaVirtual']").click()
         time.sleep(5)
+
+    def baixarMateriais(self):
+        driver = self.driver
         form = driver.find_element_by_xpath("//form[@id='formAva']")
         materiais = form.find_elements_by_xpath("//span[contains(@id,'j_id_jsp_1337189032_343')]")
         print(len(materiais))
-        for material in materiais:
-          material.find_element_by_tag_name('a').click()
-          time.sleep(5)
-        print("Acabei de baixar tudo!!")
+        # for material in materiais:
+        #   material.find_element_by_tag_name('a').click()
+        #   time.sleep(5)
+        # print("Acabei de baixar tudo!!")
         time.sleep(5)
 
-    def trocarTurma(self):
-      driver = self.driver
-      driver.find_element_by_xpath("//button[@id='formAcoesTurma:botaoTrocarTurma']").click()
-      formTurma = driver.find_element_by_xpath("//form[@id='formTurma']")
-      turmas = formTurma.find_elements_by_tag_name('a')
-      turmas[1].click()
+    def trocarDisciplina(self,cont):
+        driver = self.driver
+        driver.find_element_by_xpath("//button[@id='formAcoesTurma:botaoTrocarTurma']").click()
+        formTurma = driver.find_element_by_xpath("//form[@id='formTurma']")
+        turmas = formTurma.find_elements_by_tag_name('a')
+        turmas[cont].click()
+        time.sleep(5)
+
 
 testBot = sigaaBot('','') # TODO: inserir username e senha
 testBot.login()
